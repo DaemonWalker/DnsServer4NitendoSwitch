efeee91f945d4c8483e944c05490d4a0092da6d9
Daemon Walker <daemon0525@163.com>
2018-03-11 20:14:33 +0800

    添加项目文件。

---
+++ DnsServer4NitendoSwitch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DnsServer4NitendoSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
